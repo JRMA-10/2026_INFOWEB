bf51fff70d839c75b8dbf5359710ebf93574fad4
PARZIVAL <mendoncaalexandrejoaorafael@gmail.com>
2026-05-14 17:42:18 +0000

    novas atualizações

---
+++ TREINAMENTOS PARA A OBI /idades.py
@@ -0,0 +1,15 @@
+#cibele > camila > celeste
+
+# lista = []
+# for i in range(3):
+#     n = int(input())
+#     lista.append(n)
+
+# lista.sort()
+# print(lista[1])
+
+''''lista = []
+while len(lista) < 3: lista.append(int(input())), lista.sort()
+print(lista[1])'''
+
+a = sorted([int(input()) for _ in '123']); print(a[1])
